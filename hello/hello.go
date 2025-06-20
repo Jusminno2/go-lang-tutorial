ea2180780a276cebeee561d2d7d67199c9a7a5a2
Jusminno2 <g.urakawa@gmail.com>
2025-06-21 08:50:37 +0900

    型ヒントのお勉強

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,10 @@
-package main
-
-import "fmt"
-
-var msg = "Hello"
-
-func main() {
-	fmt.Println(msg)
-	fmt.Println("世" + "界")
-}
+//package main
+//
+//import "fmt"
+//
+//var msg = "Hello"
+//
+//func main() {
+//	fmt.Println(msg)
+//	fmt.Println("世" + "界")
+//}
