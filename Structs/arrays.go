bf35440777b8118f0a4db6f5f694d4f259d6f826
Jusminno2 <g.urakawa@gmail.com>
2025-06-24 11:41:18 +0900

    arrayは先にサイズを規定しておく（Pythonでもやるときはあるか、、）

---
+++ Structs/arrays.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [2]string // aという名前の長さ2の文字列型配列を定義
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{1, 2, 3, 4, 5, 6} // primesという長さ6のint型配列を定義し、初期値を与えて初期化
+	fmt.Println(primes)
+}
